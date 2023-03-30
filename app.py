8c32b09e0c9ebd3b0fc75c03c01b38c339c771b2
Condesay <39847380+condesay@users.noreply.github.com>
2023-03-30 11:10:12 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 # Créer une fonction pour récupérer la clé API OpenAI GPT-3 saisie par l'utilisateur
 def get_api_key():
-    api_key = st.text_input("Entrez votre clé OpenAI API:")
+    api_key = st.text_input("Enter your OpenAI API key:")
     return api_key
 
 # Créer une fonction pour récupérer la similarité entre deux textes en utilisant l'API OpenAI GPT-3
@@ -17,20 +17,22 @@ def get_similarity(text1, text2, model_engine):
         temperature=0.5,
     )
     similarity = response.choices[0].text.strip()
-    return similarity
+    # Extraire le score de similarité de la réponse générée par GPT-3
+    similarity_score = similarity.split(":")[-1].strip()
+    return similarity_score
 
 # Créer une fonction main pour gérer l'exécution du programme
 def main():
-    st.title("Recherche de Similarité entre deux textes")
+    st.title("Text Similarity Checker")
     api_key = get_api_key()
     if api_key:
         openai.api_key = api_key
         model_engine = "text-similarity-davinci-001"
-        text1 = st.text_area("Texte 1")
-        text2 = st.text_area("Texte 2")
-        if st.button("Comparez"):
-            similarity = get_similarity(text1, text2, model_engine)
-            st.write(f"La similarité entre les deux textes est {similarity}.")
+        text1 = st.text_area("Text 1")
+        text2 = st.text_area("Text 2")
+        if st.button("Compare"):
+            similarity_score = get_similarity(text1, text2, model_engine)
+            st.write(f"The similarity score between the two texts is {similarity_score}.")
 
 if __name__ == "__main__":
     main()
