b2eddf1818b59dd308c74e77f2ba6e58025c359a
Condesay <39847380+condesay@users.noreply.github.com>
2023-03-30 12:12:51 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,35 +1,36 @@
 import openai
-from openai.embeddings_utils import get_embedding, cosine_similarity
 import streamlit as st
 
-# Fonction pour récupérer la clé API OpenAI GPT-3 saisie par l'utilisateur
+# Créer une fonction pour récupérer la clé API OpenAI GPT-3 saisie par l'utilisateur
 def get_api_key():
     api_key = st.text_input("Enter your OpenAI API key:")
     return api_key
 
-# Fonction pour récupérer la similarité entre deux textes en utilisant l'API OpenAI GPT-3
+# Créer une fonction pour récupérer la similarité entre deux textes en utilisant l'API OpenAI GPT-3
 def get_similarity(text1, text2, model_engine):
-    # Récupérer les vecteurs d'embedding pour chaque texte
-    embedding1 = get_embedding(text1, model_engine)
-    embedding2 = get_embedding(text2, model_engine)
-
-    # Calculer le score de similarité en utilisant la fonction cosine_similarity
-    similarity = cosine_similarity(embedding1, embedding2)
-
+    response = openai.Completion.create(
+        engine=model_engine,
+        prompt=f"Compare the similarity between these two texts:\n\nText 1: {text1}\n\nText 2: {text2}\n\nSimilarity:",
+        max_tokens=64,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+    similarity = response.choices[0].text.strip()
     return similarity
 
-# Fonction principale pour gérer l'exécution du programme
+# Créer une fonction main pour gérer l'exécution du programme
 def main():
     st.title("Text Similarity Checker")
     api_key = get_api_key()
     if api_key:
         openai.api_key = api_key
-        model_engine = "text-babbage-001"
+        model_engine = "text-similarity-davinci-001"
         text1 = st.text_area("Text 1")
         text2 = st.text_area("Text 2")
         if st.button("Compare"):
             similarity = get_similarity(text1, text2, model_engine)
-            st.write(f"The similarity score between the two texts is {similarity}.")
+            st.write(f"The similarity between the two texts is {similarity}.")
 
 if __name__ == "__main__":
     main()
