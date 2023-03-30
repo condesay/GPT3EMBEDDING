7cdd183df2d6ed4564a9c6f955179eada26c851f
Condesay <39847380+condesay@users.noreply.github.com>
2023-03-30 15:48:48 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,32 +1,35 @@
 import streamlit as st
-from transformers import pipeline
+import torch
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
 
-# Créer une fonction pour la classification de texte en utilisant le modèle RoBERTa
-def classify_text(model, text, labels):
-    classifier = pipeline('text-classification', model=model)
-    result = classifier(text, labels=labels)[0]
-    label = result['label']
-    score = result['score']
-    return label, score
+# fonction pour effectuer la classification de texte
+def classify_text(text, labels, model_name):
+    # charger le tokenizer et le modèle pré-entraîné
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = AutoModelForSequenceClassification.from_pretrained(model_name)
 
-# Créer une fonction main pour gérer l'exécution du programme
+    # encodage du texte en entrée
+    inputs = tokenizer(text, padding=True, truncation=True, return_tensors="pt")
+
+    # classification du texte
+    outputs = model(**inputs)
+    predictions = torch.argmax(outputs.logits, dim=1)
+
+    # retourner le label prédit
+    return labels[predictions]
+
+# fonction main pour exécuter le programme
 def main():
     st.title("Text Classification")
-    text1 = st.text_area("Enter text 1")
-    text2 = st.text_area("Enter text 2")
-    label1 = st.text_input("Enter label 1")
-    label2 = st.text_input("Enter label 2")
+    text = st.text_area("Enter some text to classify")
+    label1 = st.text_input("Enter label 1:")
+    label2 = st.text_input("Enter label 2:")
     if st.button("Classify"):
-        model_name = "textattack/roberta-base-SST-2"
+        # classification du texte saisi
         labels = [label1, label2]
-        label1_score = classify_text(model_name, text1, labels)[1]
-        label2_score = classify_text(model_name, text2, labels)[1]
-        if label1_score > label2_score:
-            st.write(f"Text 1 is classified as {label1} with a confidence score of {label1_score}.")
-            st.write(f"Text 2 is classified as {label2} with a confidence score of {label2_score}.")
-        else:
-            st.write(f"Text 1 is classified as {label2} with a confidence score of {label2_score}.")
-            st.write(f"Text 2 is classified as {label1} with a confidence score of {label1_score}.")
+        model_name = "distilbert-base-uncased-finetuned-sst-2-english" # modèle pré-entraîné pour la classification binaire
+        label = classify_text(text, labels, model_name)
+        st.write(f"The text is classified as '{label}'.")
 
 if __name__ == "__main__":
     main()
