88ed6b32b26216af4c77235d9f7659e87a3ec963
Condesay <39847380+condesay@users.noreply.github.com>
2023-03-30 15:41:22 +0200

    Update app.py

--- app.py
+++ app.py
@@ -12,14 +12,21 @@ def classify_text(model, text, labels):
 # Créer une fonction main pour gérer l'exécution du programme
 def main():
     st.title("Text Classification")
-    text = st.text_area("Enter text to classify")
-    label1 = st.selectbox("Select label 1", ["Positive", "Negative"])
-    label2 = st.selectbox("Select label 2", ["Positive", "Negative"])
+    text1 = st.text_area("Enter text 1")
+    text2 = st.text_area("Enter text 2")
+    label1 = st.text_input("Enter label 1")
+    label2 = st.text_input("Enter label 2")
     if st.button("Classify"):
         model_name = "textattack/roberta-base-SST-2"
         labels = [label1, label2]
-        label, score = classify_text(model_name, text, labels)
-        st.write(f"The text is classified as {label} with a confidence score of {score}.")
+        label1_score = classify_text(model_name, text1, labels)[1]
+        label2_score = classify_text(model_name, text2, labels)[1]
+        if label1_score > label2_score:
+            st.write(f"Text 1 is classified as {label1} with a confidence score of {label1_score}.")
+            st.write(f"Text 2 is classified as {label2} with a confidence score of {label2_score}.")
+        else:
+            st.write(f"Text 1 is classified as {label2} with a confidence score of {label2_score}.")
+            st.write(f"Text 2 is classified as {label1} with a confidence score of {label1_score}.")
 
 if __name__ == "__main__":
     main()
