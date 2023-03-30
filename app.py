982ad72399884fde8257fc8c220b6f04d37c6967
Condesay <39847380+condesay@users.noreply.github.com>
2023-03-30 15:35:41 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,46 +1,22 @@
 import streamlit as st
-import torch
-from transformers import AutoTokenizer, AutoModelForSequenceClassification
+from transformers import pipeline
 
-# Créer une fonction pour charger le modèle et le tokenizer
-@st.cache(allow_output_mutation=True)
-def load_model(model_name):
-    model = AutoModelForSequenceClassification.from_pretrained(model_name)
-    tokenizer = AutoTokenizer.from_pretrained(model_name)
-    return model, tokenizer
+# Créer une fonction pour la classification de texte en utilisant le modèle RoBERTa
+def classify_text(model, text):
+    classifier = pipeline('sentiment-analysis', model=model)
+    result = classifier(text)[0]
+    label = result['label']
+    score = result['score']
+    return label, score
 
 # Créer une fonction main pour gérer l'exécution du programme
 def main():
-    st.title("Document Classification")
-    
-    # Sélectionner le modèle à utiliser
-    model_name = st.selectbox("Select a model", ["bert-base-uncased", "textattack/roberta-base-SST-2"])
-    
-    # Charger le modèle et le tokenizer
-    model, tokenizer = load_model(model_name)
-    
-    # Entrer le texte à classifier
-    text = st.text_area("Enter some text")
-    
-    # Sélectionner les classes à classifier
-    class1 = st.text_input("Enter class 1")
-    class2 = st.text_input("Enter class 2")
-    
-    # Classer le texte lorsque l'utilisateur clique sur le bouton "Classify"
+    st.title("Text Classification")
+    text = st.text_area("Enter text to classify")
+    model_name = "textattack/roberta-base-SST-2"
     if st.button("Classify"):
-        # Encoder le texte en utilisant le tokenizer
-        encoded_text = tokenizer(text, padding=True, truncation=True, max_length=128, return_tensors='pt')
-        
-        # Faire une prédiction en utilisant le modèle
-        with torch.no_grad():
-            outputs = model(encoded_text['input_ids'], token_type_ids=None, attention_mask=encoded_text['attention_mask'])
-            prediction = torch.argmax(outputs[0]).item()
-        
-        # Afficher le résultat
-        if prediction == 0:
-            st.write(class1)
-        else:
-            st.write(class2)
+        label, score = classify_text(model_name, text)
+        st.write(f"The text is classified as {label} with a confidence score of {score}.")
 
 if __name__ == "__main__":
     main()
